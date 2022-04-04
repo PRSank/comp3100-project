c5e1de3232dd95cc882505437e042ff0edfed1d0
Prateek <prateek.sankepalli@students.mq.edu.au>
2022-04-04 13:13:50 +1000

    cleaned code and improved readability

--- MyClient.java
+++ MyClient.java
@@ -4,174 +4,161 @@ import java.net.*;
 public class MyClient {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("127.0.0.1",50000);
+            Socket s = new Socket("127.0.0.1", 50000);
             BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            
-            String str = "",str2="";
 
+            String sendSTR = "", recieveSTR = ""; // Initialises the String objects used to hold sending and recieved
+                                                  // messages
 
-            handshake(str, str2, din, dout);
+            handshake(sendSTR, recieveSTR, din, dout); // Handshake
 
-        
+            // Initialising variables
+            String lType = ""; // Largest Server Name
+            int lCore = 0; // Largest Server Core size
+            int sCount = 0; // Count of largest server
+            boolean checked = false; // Checks if the client has already requested server information
 
-            //Initialising constants
-            String lType="";
-            int lCore = 0;
-            int sCount = 0;
-            boolean checked = false;
+            int job = 0; // jobID always start at 0
+            int serverID = 0; // ServerID always start from 0
 
-            int job = 0;
-            int serverID = 0;
-
-
-            while(!str2.equals("NONE")){
-                str = "REDY\n";
-                dout.write((str).getBytes());
+            while (!recieveSTR.equals("NONE")) {
+                // Requesting job information
+                sendSTR = "REDY\n";
+                dout.write((sendSTR).getBytes());
                 dout.flush();
 
-                str2=din.readLine();
-                System.out.println("Server says:"+str2);
-                
+                recieveSTR = din.readLine();
+                System.out.println("Server says:" + recieveSTR);
 
-                String[] tester = str2.split(" ");
-                //findLargestServer(checked,str, str2, din, dout, lCore, lType, sCount);
+                String[] jobCheck = recieveSTR.split(" ");
 
-                if(checked == false) {
-                    // findLargestServer(str, str2, din, dout, lCore, lType, sCount);
-                    str = "GETS All\n";
-                    dout.write((str).getBytes());
+                if (checked == false) {
+                    // Request server information
+                    sendSTR = "GETS All\n";
+                    dout.write((sendSTR).getBytes());
                     dout.flush();
 
-                    str2=din.readLine();
-                    System.out.println("Server says:"+str2);
+                    recieveSTR = din.readLine();
+                    System.out.println("Server says:" + recieveSTR);
 
-                    //Finding amount of servers
-                    String[] dataArr = str2.split(" ");
+                    // Finding amount of servers
+                    String[] dataArr = recieveSTR.split(" ");
                     int nRecs = Integer.parseInt(dataArr[1]);
 
-                    //end
-                    str = "OK\n";
-                    dout.write((str).getBytes());
-                    dout.flush();
+                    okACK(sendSTR, recieveSTR, din, dout);
 
-                    //C/ataloging Server sizes
-                    for(int i =0; i < nRecs;++i) {
-                        str2=din.readLine();
-                        String[] str3 = str2.split(" ");
+                    // Cataloging Server sizes
+                    for (int i = 0; i < nRecs; ++i) {
+                        recieveSTR = din.readLine();
+                        String[] serverINFO = recieveSTR.split(" ");
 
-                        if(lCore < Integer.parseInt(str3[4])){
-                            lCore = Integer.parseInt(str3[4]);
-                            lType = str3[0];
+                        if (lCore < Integer.parseInt(serverINFO[4])) {
+                            lCore = Integer.parseInt(serverINFO[4]);
+                            lType = serverINFO[0];
                         }
 
-                        if(lType.equals(str3[0])) {
-                            sCount = 1 + Integer.parseInt(str3[1]);
+                        if (lType.equals(serverINFO[0])) {
+                            sCount = 1 + Integer.parseInt(serverINFO[1]);
                         }
-    
-                        System.out.println("Server says:"+str2);
+
+                        System.out.println("Server says:" + recieveSTR);
                     }
-                    System.out.println("Largest Server Type: " +lType+ " Cores: " +lCore +" Count "+ sCount);
+                    System.out.println("Largest Server Type: " + lType + " Cores: " + lCore + " Count " + sCount);
 
-                    str = "OK\n";
-                    dout.write((str).getBytes());
-                    dout.flush();
+                    okACK(sendSTR, recieveSTR, din, dout);
 
-                    str2=din.readLine();
-                    System.out.println("Server says:"+str2);
+                    recieveSTR = din.readLine();
+                    System.out.println("Server says:" + recieveSTR);
 
                     checked = true;
 
-
                 }
 
+                // Reading job message and schedule
+                if (jobCheck[0].equals("JOBN")) {
+                    // schedule job
+                    schdAction(sendSTR, recieveSTR, din, dout, job, lType, serverID);
 
-                if(tester[0].equals("JOBN")){
-                    
-                    schdAction(str, str2, din, dout, job, lType, serverID);
-
-                    job++;
+                    job++; // increments job and server to next appropriate job/server
                     serverID++;
-                    if(serverID >= sCount){
+                    if (serverID >= sCount) {
                         serverID = 0;
                     }
                 }
-
-                 
             }
-            quitAction(str, str2, din, dout, s); //quits and closes socket
-        } catch(Exception e){System.out.println(e);}
+            // Quitting and disconnecting from server
+            quitAction(sendSTR, recieveSTR, din, dout, s); // quits and closes socket
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    static void findLargestServer(String send, String recieve, BufferedReader din,DataOutputStream dout,int lCore, String lType, int sCount){
-        try {
-            
-
-        } catch(Exception e){System.out.println(e);}
-    }
-    //completes handshake to connect with server
-    static void handshake(String send, String recieve, BufferedReader din,DataOutputStream dout) {
+    // completes handshake to connect with server
+    static void handshake(String send, String recieve, BufferedReader din, DataOutputStream dout) {
         try {
             send = "HELO\n";
             dout.write((send).getBytes());
             dout.flush();
-            
 
-            recieve=din.readLine();
-            System.out.println("Server says:"+recieve);
-            
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
             send = "AUTH prateek\n";
             dout.write((send).getBytes());
             dout.flush();
 
-            recieve=din.readLine();
-            System.out.println("Server says:"+recieve);
-            
-        } catch(Exception e){System.out.println(e);}
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    //sends a quit message and closes socket
-    static void quitAction(String send, String recieve, BufferedReader din,DataOutputStream dout, Socket s) {
+    // sends a quit message and closes socket
+    static void quitAction(String send, String recieve, BufferedReader din, DataOutputStream dout, Socket s) {
         try {
             send = "QUIT\n";
             dout.write((send).getBytes());
             dout.flush();
 
-            recieve=din.readLine();
-            System.out.println("Server says:"+recieve);
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
 
             dout.close();
             s.close();
 
-        } catch(Exception e){System.out.println(e);}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    //sends a scheduling message
-    static void schdAction(String send, String recieve, BufferedReader din,DataOutputStream dout, int job , String lType, int serverID) {
+    // sends a scheduling message
+    static void schdAction(String send, String recieve, BufferedReader din, DataOutputStream dout, int job,
+            String lType, int serverID) {
         try {
-            send = "SCHD " + job +" "+ lType +" "+ serverID+ "\n";
+            send = "SCHD " + job + " " + lType + " " + serverID + "\n";
             dout.write((send).getBytes());
             dout.flush();
 
-            recieve=din.readLine();
-            System.out.println("Server says:"+recieve);
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
 
-        } catch(Exception e){System.out.println(e);}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    
+    // Sends an OK acknowledment
+    static void okACK(String send, String recieve, BufferedReader din, DataOutputStream dout) {
+        try {
+            send = "OK\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
